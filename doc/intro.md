6b18a625263871ee3e746def20661fe0800a6bba
tangrammer <juanantonioruz@gmail.com>
2014-01-30 18:21:57 +0100

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to heroku-clojure-rest
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/heroku_clojure_rest/core_test.clj
@@ -0,0 +1,7 @@
+(ns heroku-clojure-rest.core-test
+  (:require [clojure.test :refer :all]
+            [heroku-clojure-rest.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
