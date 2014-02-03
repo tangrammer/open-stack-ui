840d1222481c278bb25546be12afc3f2fccc0672
tangrammer <juanantonioruz@gmail.com>
2014-02-03 21:09:47 +0100

    working with rest OM client

--- src/clj/heroku_clojure_rest/core.clj
+++ src/clj/heroku_clojure_rest/core.clj
@@ -2,25 +2,28 @@
     (:use [open-stack-wrapper.core])
    (:require [cemerick.austin.repls :refer (browser-connected-repl-js)]
              [net.cgrand.enlive-html :as enlive]
-             [liberator.core :refer [resource defresource]]
-            [ring.middleware.params :refer [wrap-params]]
-            [ring.adapter.jetty :refer [run-jetty]]
-            [compojure.core :refer [defroutes ANY]]
+
+             [ring.middleware.params :refer [wrap-params]]
+             [ring.util.response :as resp]
+             [ring.adapter.jetty :refer [run-jetty]]
+             [ring.middleware.json :as middleware]
+             [compojure.core :refer [defroutes ANY POST GET routes]]
+             [compojure.handler :as comphand]
             [compojure.route :refer (resources)]
             [clojure.java.io :as io]))
 
 (def username "facebook1428467850")
 (def password "3a34gc72")
-
+(def url "http://8.21.28.222:5000")
 
 (comment  "to start:
  * cljsbuild once to generate the js
  * brepl evaluate both lines and reload browser
  * on the brepl enter this line (js/alert 'here')"
 
-  (def repl-env (reset! cemerick.austin.repls/browser-repl-env
+          (def repl-env (reset! cemerick.austin.repls/browser-repl-env
                                (cemerick.austin/repl-env)))
-         (cemerick.austin.repls/cljs-repl repl-env)
+          (cemerick.austin.repls/cljs-repl repl-env)
 
          )
 
@@ -32,28 +35,58 @@
   []
   [:body] (enlive/append
             (enlive/html [:script (browser-connected-repl-js)])))
+
+(def posts (ref []))
+
+
 (defroutes app
   (resources "/")
   (resources "/out")
 
+  (GET "/dev" [] (page))
 
   (ANY "/" req (page))
-  (ANY "/tokens" [] (resource :available-media-types ["application/json"]
-                              :handle-ok (fn [ctx]  {:success true :token-id (get-in (tokens username password) [:access :token :id])})))
-  (ANY  "/endpoints/:tenant" [tenant]  (resource :allowed-methods [:post :get]
-                                          :available-media-types ["application/json"]
-                                          :handle-ok (fn [_]  {:success true :endpoints (structured-endpoints (endpoints username password tenant))}))))
+
+
+ )
+
+(defroutes app1
+
+  #_(comment
+           (ANY "/tokens" [] (resource :available-media-types ["application/json"]
+                                       :handle-ok (fn [ctx]  {:success true :token-id (get-in (tokens url username password) [:access :token :id])})))
+           (ANY  "/endpoints/:tenant" [tenant]  (resource :allowed-methods [:post :get]
+                                                          :available-media-types ["application/json"]
+                                                          :handle-ok (fn [_]  {:success true :endpoints (structured-endpoints (endpoints url username password tenant))}))))
+
+  (ANY "/connect" [url username password]
+       (println url username password)
+       (resp/response {:success true :token-id (get-in (tokens url username password) [:access :token :id])}))
+  (ANY "/tenants" [url token-id]
+       (println url token-id)
+       (resp/response (tenants url token-id)))
+    (ANY "/endpoints" [url username password tenantname]
+       (println url username password tenantname)
+       (resp/response {:success true :endpoints (structured-endpoints (endpoints url username password tenantname))}))
+)
+
+(def admin-routes
+  (-> app1
+      (middleware/wrap-json-response  {:keywords? true})
+))
 
 (declare server)
 
 (def handler
-  (-> app
-      (wrap-params)))
+  (comphand/api (routes
+                 app
+                 admin-routes)
+))
 (comment
  (run-jetty #'handler {:port 5000}))
 
 (defn -main [port]
-   (def server (run-jetty #'handler {:port (Integer. port) :join? false :auto-reload? true :reload-paths "/resources"}))
+   (def server (run-jetty #'handler {:port (Integer. port) :join? false }))
 )
 
 (defn run []
--- src/cljs/heroku/index.cljs
+++ src/cljs/heroku/index.cljs
@@ -1,11 +1,19 @@
 (ns heroku.index
   (:require-macros [cljs.core.async.macros :refer [go]])
   (:require
-       [goog.net.XhrIo :as xhr]
-       [om.core :as om :include-macros true]
-       [om.dom :as dom :include-macros true]
-       [clojure.browser.repl]
-       [cljs.core.async :refer [put! chan <!]]))
+   [ajax.core :refer [GET POST]]
+   [om.core :as om :include-macros true]
+   [om.dom :as dom :include-macros true]
+   [clojure.browser.repl]
+   [cljs.core.async :refer [put! chan <!]]))
+
+(def app-state (atom {:text "Hello prototype!"}))
+(def username "facebook1428467850")
+(def password "3a34gc72")
+(def url "http://8.21.28.222:5000")
+
+
+
 
 (defn widget [data owner]
   (reify
@@ -14,23 +22,177 @@
       (dom/h1 nil (:text data)))))
 
 
-(defn GET [url]
-  (xhr/send url
-            (fn [event]
-              (let [res (-> event .-target .getResponseText)]
-                (js/alert res)
-                (om/root {:text res} widget (. js/document (getElementById "my-app")))
+(defn handler2 [response]
+  (.log js/console (str response))
+  )
+
+
+
+
+                                        ;(om/root {:text "Hello prototype!"} widget (. js/document (getElementById "my-app")))
+
+(def o (atom nil))
+
+(defn handler [response]
+  (.log js/console (str response))
+  )
+
+(defn error-handler [{:keys [status status-text]}]
+  (.log js/console (str "something bad happened: " status " " status-text)))
+
+
+(def connect-channel (chan))
+
+(defn try-to-connect [e data owner]
+  (let [url (.-value (om/get-node owner "url"))
+        password (.-value (om/get-node owner "password"))
+        username (.-value (om/get-node owner "username"))
+        ]
+    (swap! app-state assoc :url url :password password :username username)
+    #_(.log js/console url password username)
+
+
+    (GET
+     "/connect"
+     {:handler (fn [response] (if (:success response)
+                               (put! connect-channel (:token-id response))
+                               (om/root (swap! app-state assoc :text "error!") widget (. js/document (getElementById "my-app")))))
+      :error-handler error-handler
+      :params {:url url :password password :username username}
+      :response-format :json
+      :keywords? true})
+
+    ))
+
+(defn tenant-view [tenant owner]
+  (reify
+    om/IRender
+    (render [this]
+      (dom/li #js {:className "list-group-item"} (:name tenant)))))
+
+(defn tenants-view [app owner]
+  (reify
+    om/IRender
+    (render [this]
+      (dom/div nil
+               (dom/h2 nil "Tenants list")
+               (apply dom/ul #js {:className "list-group"}
+                      (om/build-all tenant-view (:tenants app)))))))
+
+(defn render-tenants [data]
+
+  (om/root data tenants-view (. js/document (getElementById "my-app"))))
+
+
+
+
+
+(defn get-tenants [token-id]
+  (GET
+   "/tenants"
+   {:handler (fn [response]
+
+               (if (:success response)
+                 (do
+                   (swap! app-state assoc :tenants (:tenants response))
+                   (render-tenants app-state))
+                 (do
+                   (swap! app-state assoc :tenants (:tenants response))
+                   (render-tenants app-state))
+
+                 )
+               )
+    :error-handler error-handler
+    :params {:url (:url @app-state) :token-id token-id}
+    :response-format :json
+    :keywords? true}))
+
+
+(go
+ (loop []
+   (let [token (<! connect-channel)]
+     (println token)
+     (get-tenants token)
+     (recur))
+
+   )
+
+ )
+
+(comment (GET
+          "/endpoints"
+          {:handler (fn [response]
+                      (if (:success response)
+                        (swap! app-state assoc :enpoints (:endpoints response))
+                        (swap! app-state assoc :endpoints (:endpoints response)) ;TODO                (js/alert "no tenants!\n" response)
+                        )
+                      )
+           :error-handler error-handler
+           :params {:url (:url @app-state) :username (:username @app-state) :password (:password @app-state) :tenantname (:name (first (:tenants @app-state)))}
+           :response-format :json
+           :keywords? true}))
+
+(defn widget-form [data owner]
+  (reify
+    om/IRender
+    (render [this]
+
+      (dom/form #js {:className "form-signin" :role "form" }
+
+                (dom/h2 #js {:className "form-signin-heading"} "Try a connection")
+                (dom/input #js {:ref "url" :type "url" :className "form-control" :placeholder "Connection Url" :required true :autoFocus true :value url} )
+                (dom/input #js {:ref "username" :type "text" :className "form-control" :placeholder "User Name" :required true :value username} )
+                (dom/input #js {:ref "password" :type "password" :className "form-control" :placeholder "Password" :required true :value password}  )
+                (dom/button #js {:className "btn btn-lg btn-primary btn-block" :type "button" :onClick #(try-to-connect % data owner)} "Connect!")
+                ))))
+                                        ;(om/root app-state widget-form (. js/document (getElementById "my-app")))
+
+(defn widget-form-tenant [data owner]
+  (reify
+    om/IRender
+    (render [this]
+      (dom/form #js {:className "form-signin" :role "form" }
+
+                (dom/h2 #js {:className "form-signin-heading"} "Try a tenant")
+                (dom/input #js {:ref "url" :type "url" :className "form-control" :placeholder "Connection Url" :required true :autoFocus true :value url} )
+                (dom/input #js {:ref "username" :type "text" :className "form-control" :placeholder "User Name" :required true :value username} )
+                (dom/input #js {:ref "password" :type "password" :className "form-control" :placeholder "Password" :required true :value password}  )
+                (dom/input #js {:ref "tenant" :type "text" :className "form-control" :placeholder "Tenant name" :required true :value username}  )
+                (dom/button #js {:className "btn btn-lg btn-primary btn-block" :type "button" :onClick #(try-to-connect % data owner)} "Connect!")
                 ))))
 
+(defn render-connect []
+  (om/root (swap! app-state assoc :connection :base) widget-form (. js/document (getElementById "my-app"))))
+(defn render-connect-tenant []
+  (om/root (swap! app-state assoc :connection :tenant)  widget-form-tenant (. js/document (getElementById "my-app"))))
+
+(render-connect)
+
+(comment
+  (om/root (swap! app-state assoc :text "holooo") widget (. js/document (getElementById "my-app")))
+
+  (defn hello
+    []
+    (js/alert "hello"))
+
+  (defn whoami
+    []
+    (.-userAgent js/navigator))
+  )
 
 
-(om/root {:text "Hello prototype!"} widget (. js/document (getElementById "my-app")))
 
+(defn navbar [data owner]
+  (reify
+    om/IRender
+    (render [this]
+      (dom/nav #js {:className "navbar navbar-default" :role "navigation"}
+               (dom/div #js {:className "collapse navbar-collapse"}
+                        (dom/ul #js {:className "nav navbar-nav"}
 
-(defn hello
-  []
-  (js/alert "hello"))
-
-(defn whoami
-  []
-  (.-userAgent js/navigator))
+                                (dom/li #js {:className (when (= :base (:connection data)) "active") }
+                                        (dom/a #js {:href "#" :onClick render-connect} "Try a connection"))
+                                (dom/li #js {:className (when (= :tenant (:connection data)) "active")  }
+                                        (dom/a #js {:href "#" :onClick render-connect-tenant} "Connect to a tennant"))))))))
+(render-connect)
+(om/root app-state navbar (. js/document (getElementById "my-nav")))
