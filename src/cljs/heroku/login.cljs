19a3de529fe494dcd903266251866328f987c5fa
tangrammer <juanantonioruz@gmail.com>
2014-02-12 13:06:01 +0100

    gitignore login

--- src/cljs/heroku/login.cljs
+++ src/cljs/heroku/login.cljs
@@ -1,9 +1,5 @@
 (ns heroku.login)
 
-;(def username "admin")
-;(def password "password")
-;(def url "http://192.168.1.11:5000")
-
-(def username "facebook1428467850")
-(def password "3a34gc72")
-(def url "http://8.21.28.222:5000")
+(def username "admin")
+(def password "password")
+(def url "http://192.168.1.11:5000")
